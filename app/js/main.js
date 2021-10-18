51a25abbe34522ed3389ca976428dfc3ebaccfeb
VladimirLakodei <vovalak@mail.ru>
2021-10-18 19:07:37 +0300

    moved egg

---
+++ app/js/main.js
@@ -0,0 +1,33 @@
+(function() {
+  var mousePosition;
+  var offset = [0,0];
+  var div;
+  var isDown = false;
+
+  div = document.querySelector("#egg");
+
+  div.addEventListener('mousedown', function(e) {
+    isDown = true;
+    offset = [
+      div.offsetLeft - e.clientX,
+      div.offsetTop - e.clientY
+    ];
+  }, true);
+
+  document.addEventListener('mouseup', function() {
+    isDown = false;
+  }, true);
+
+  document.addEventListener('mousemove', function(event) {
+    event.preventDefault();
+    if (isDown) {
+      mousePosition = {
+        x : event.clientX,
+        y : event.clientY
+      };
+
+      div.style.left = (mousePosition.x + offset[0]) + 'px';
+      div.style.top  = (mousePosition.y + offset[1]) + 'px';
+    }
+  }, true);
+})();
